057e0c83a63a2bbb746a0b9e4c038b689ef7c236
AlissonSteffens <alisson.steffens@hotmail.com>
2017-05-24 21:23:29 -0300

    suicidio

--- script.js
+++ script.js
@@ -6,30 +6,59 @@ var Architect = neataptic.Architect;
 var Trainer = neataptic.Trainer;
 
 
-// var network = new Architect.Perceptron(1024 * 3, 120 ,1);
+
 $( document ).ready(function() {
 
-  // network.train(trainingSet, {
-  //   iterations: 10000000,
-  //   error: 0.0001,
-  //   rate: 0.2
-  // });
+
 });
 var j = 0;
-// $('#actual').click(function( event  ) {
-//   $("#actual").broiler(function(color) {
-//     // do what ever you want with the color object, for example an alert
-//     alert("red:"+color.r+" green:"+color.g+" blue:"+color.b+" alpha:"+color.a);
-//   });
-// });
+
 $('#test').click(function( event  ) {
   event.preventDefault();
   j++
+  var network = new Architect.Perceptron(1024 * 4, 120 ,1);
 
-
-
-
-  $('#actual').attr('src','resources/img/f1/full/'+j+'.png')
+  var vet = []
+  //$('#actual').attr('src','resources/img/f1/full/'+j+'.png')
+  for (var i = 1; i <= 10; i++) {
+    $('#actual').attr('src','http://beholderdev.github.io/Neur/resources/img/f1/'+i+'.png')
+    var img = document.getElementById('actual');
+    var canvas = document.createElement('canvas');
+    var context = canvas.getContext('2d');
+    canvas.width = img.width;
+    canvas.height = img.height;
+    context.drawImage(img, 0, 0 );
+    var myData = context.getImageData(0, 0, img.width, img.height);
+    vet[i-1] = myData
+  }
+  // $('#actual').attr('src','https://beholderdev.github.io/Neur/resources/img/f1/'+j+'.png')
+  // var img = document.getElementById('actual');
+  // var canvas = document.createElement('canvas');
+  // var context = canvas.getContext('2d');
+  // canvas.width = img.width;
+  // canvas.height = img.height;
+  // context.drawImage(img, 0, 0 );
+  // var myData = context.getImageData(0, 0, img.width, img.height);
+  // console.log(myData);
+  var trainingSet = [
+    { input: vet[0], output: [1] },
+    { input: vet[1], output: [2] },
+    { input: vet[2], output: [3] },
+    { input: vet[3], output: [4] },
+    { input: vet[4], output: [5] },
+    { input: vet[5], output: [6] },
+    { input: vet[6], output: [7] },
+    { input: vet[7], output: [8] },
+    { input: vet[8], output: [9] },
+    { input: vet[9], output: [10] }
+  ];
+  network.train(trainingSet, {
+    // log: 10,
+    iterations: 10000,
+    error: 0.0001,
+    rate: 0.2
+  });
+  $('#actual').attr('src','http://beholderdev.github.io/Neur/resources/img/f2/'+2+'.png')
   var img = document.getElementById('actual');
   var canvas = document.createElement('canvas');
   var context = canvas.getContext('2d');
@@ -37,19 +66,9 @@ $('#test').click(function( event  ) {
   canvas.height = img.height;
   context.drawImage(img, 0, 0 );
   var myData = context.getImageData(0, 0, img.width, img.height);
-  console.log(myData);
 
-
-  // canvas.width = img.width;
-  // canvas.height = img.height;
-  // canvas.getContext('2d').drawImage(img, 0, 0, img.width, img.height);
-  // // var context  = canvas.getContext('2d');
-  // // for(var i = 0)
-  // var pixelData = canvas.getContext('2d').getImageData(16, 16, 1, 1).data;
-  // // var myData = context.getImageData(0, 0, event.offsetX, event.offsetY);
-  // console.log(pixelData)
-
-
+  var out = network.activate(myData);
+  console.log(out)
   // var i1 = $('#input1').val()
   // var i2 = $('#input2').val()
   // var out = network.activate([i1,i2])

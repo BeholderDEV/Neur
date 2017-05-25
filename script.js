16149286bbdc4339e6e4044134793cfd6c7e6daf
Unknown <acpluschkat@hotmail.com>
2017-05-25 18:46:33 -0300

    XAMPP é a solução e normalizando valores

--- script.js
+++ script.js
@@ -5,24 +5,63 @@ var Methods = neataptic.Methods;
 var Architect = neataptic.Architect;
 var Trainer = neataptic.Trainer;
 
+function tryShit(){
+  var network = new Architect.Perceptron(2, 3 ,1);
+  var trainingSet = [
+    { input: [0, 0], output: [0] },
+    { input: [1, 1], output: [2] }
+  ];
+  network.train(trainingSet, {
+    // log: 10,
+    iterations: 1000,
+    error: 0.0001,
+    rate: 0.2
+  });
+  var out = network.activate([0, 0]);
+  console.log("Result com valores maiores que um: " + out)
+
+  var out = network.activate([1, 1]);
+  console.log("Result com valores maiores que um: " + out)
+
+  var trainingSet = [
+    { input: [0, 0], output: [0 / 2] },
+    { input: [1, 1], output: [2 / 2] }
+  ];
+  network.train(trainingSet, {
+    // log: 10,
+    iterations: 1000,
+    error: 0.0001,
+    rate: 0.2
+  });
+
+  var out = network.activate([0, 0]) * 2;
+  console.log("Result com valores normalizados: " + out)
+
+  var out = network.activate([1, 1]) * 2;
+  console.log("Result com valores normalizados: " + out)
+}
+
 
 
 $( document ).ready(function() {
 
-
 });
+
 var j = 0;
+var numeroImagens = 145;
 
 $('#test').click(function( event  ) {
   event.preventDefault();
-  console.log('v0.1')
-  j++
-  var network = new Architect.Perceptron(1024, 20 ,1);
+
+  tryShit()
+
+  // j++
+  var network = new Architect.Perceptron(1024, 70 ,1);
 
   var vet = []
   //$('#actual').attr('src','resources/img/f1/full/'+j+'.png')
   for (var i = 1; i <= 10; i++) {
-    $('#actual').attr('src','http://beholderdev.github.io/Neur/resources/img/f1/low/'+i+'.jpg')
+    $('#actual').attr('src','resources/img/f1/low/'+i+'.jpg')
     var img = document.getElementById('actual');
     var canvas = document.createElement('canvas');
     var context = canvas.getContext('2d');
@@ -37,35 +76,26 @@ $('#test').click(function( event  ) {
     vet[i-1] = newData
     console.log(i)
   }
-  // $('#actual').attr('src','https://beholderdev.github.io/Neur/resources/img/f1/'+j+'.png')
-  // var img = document.getElementById('actual');
-  // var canvas = document.createElement('canvas');
-  // var context = canvas.getContext('2d');
-  // canvas.width = img.width;
-  // canvas.height = img.height;
-  // context.drawImage(img, 0, 0 );
-  // var myData = context.getImageData(0, 0, img.width, img.height);
-  // console.log(myData);
   var trainingSet = [
-    { input: vet[0], output: [1] },
-    { input: vet[1], output: [2] },
-    { input: vet[2], output: [3] },
-    { input: vet[3], output: [4] },
-    { input: vet[4], output: [5] },
-    { input: vet[5], output: [6] },
-    { input: vet[6], output: [7] },
-    { input: vet[7], output: [8] },
-    { input: vet[8], output: [9] },
-    { input: vet[9], output: [10] }
+    { input: vet[0], output: [1 / numeroImagens] },
+    { input: vet[1], output: [2 / numeroImagens] },
+    { input: vet[2], output: [3 / numeroImagens] },
+    { input: vet[3], output: [4 / numeroImagens] },
+    { input: vet[4], output: [5 / numeroImagens] },
+    { input: vet[5], output: [6 / numeroImagens] },
+    { input: vet[6], output: [7 / numeroImagens] },
+    { input: vet[7], output: [8 / numeroImagens] },
+    { input: vet[8], output: [9 / numeroImagens] },
+    { input: vet[9], output: [10 / numeroImagens] }
   ];
   network.train(trainingSet, {
     // log: 10,
-    iterations: 10,
+    iterations: 100,
     error: 0.0001,
     rate: 0.2
   });
   console.log('acabo')
-  $('#actual').attr('src','http://beholderdev.github.io/Neur/resources/img/f2/low/'+8+'.jpg')
+  $('#actual').attr('src','resources/img/f2/low/'+8+'.jpg')
   var img = document.getElementById('actual');
   var canvas = document.createElement('canvas');
   var context = canvas.getContext('2d');
@@ -77,11 +107,6 @@ $('#test').click(function( event  ) {
   for(var j = 0; j < 1024 ; j++){
     newData[j] = myData[j*3]
   }
-  var out = network.activate(newData);
-  console.log(out)
-  // var i1 = $('#input1').val()
-  // var i2 = $('#input2').val()
-  // var out = network.activate([i1,i2])
-  // $('#output').val(out)
+  var out = network.activate(newData) * numeroImagens;
+  console.log("Testando imagens 8, resultado da rede neural: " + out)
 });
-//drawGraph(network.graph(400, 400), '.draw', false);

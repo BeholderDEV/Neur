d18ab7c1a2beaefb9b6131feebe9432305a172cc
Augus-top <acpluschkat@hotmail.com>
2017-05-29 08:01:00 -0300

    Bulba, Charmander e Squirtle networks

--- script.js
+++ script.js
@@ -5,9 +5,10 @@ var Methods = neataptic.Methods;
 var Architect = neataptic.Architect;
 var Trainer = neataptic.Trainer;
 var j = 0;
-var numeroImagens = 3;
+var numeroImagens = 10;
 var network;
-var imgQuality = 'low';
+var imgQuality = 'testLow';
+var testSet = 'f1';
 
 $( document ).ready(function() {
 
@@ -43,9 +44,10 @@ function validateTest(i, numeroImageTest, set, canvas, context){
     for(var j = 0; j < baseImage.width * baseImage.height ; j++){
       newData[j] = myData.data[j * 4];
     }
-    var out = network.activate(newData) * numeroImagens;
+    // var out = network.activate(newData) * numeroImagens;
+    var out = network.activate(newData);
     console.log('Testando Imagem ' + i + ': ' + out);
-    validateTest(i + 1, numeroImageTest, 'f2', canvas, context);
+    validateTest(i + 1, numeroImageTest, set, canvas, context);
   });
 
 }
@@ -63,7 +65,7 @@ function loadImage(i, set, quality, callback){
 }
 
 function prepareImageArray(i, numeroImageTest, set, trainingSet, canvas, context, callback){
-  if(i === numeroImageTest + 1){
+  if(i === numeroImagens + 1){
     callback(trainingSet);
     return;
   }
@@ -80,14 +82,27 @@ function prepareImageArray(i, numeroImageTest, set, trainingSet, canvas, context
     for(var j = 0; j < baseImage.width * baseImage.height; j++){
       newData[j] = myData.data[j * 4];
     }
-    trainingSet[i - 1] = {input: newData, output: [i / numeroImagens]};
+    // if(i === 1){
+    //   trainingSet[i - 1] = {input: newData, output: [1, 0, 0]};
+    // }else if (i === 2){
+    //   trainingSet[i - 1] = {input: newData, output: [0, 1, 0]};
+    // }else if(i === 3){
+    //   trainingSet[i - 1] = {input: newData, output: [0, 0 ,1]};
+    // }
+    if(i === 4){
+      trainingSet[i - 1] = {input: newData, output: [1]};
+    }else{
+      trainingSet[i - 1] = {input: newData, output: [0]};
+    }
+    
+    // trainingSet[i - 1] = {input: newData, output: [i / numeroImagens]};
     prepareImageArray(i + 1, numeroImageTest, set, trainingSet, canvas, context, callback);
   });
 
 }
 
 $('#load').click(function(event){
-  $.getJSON("resources/trained_network/Neur32.json", function(json) {
+  $.getJSON("resources/trained_network/squirtleNeur.json", function(json) {
       network = Network.fromJSON(json);
       console.log("Loaded JSON");
   });
@@ -108,22 +123,26 @@ $('#test').click(function(event){
   }
   var canvas = document.createElement('canvas');
   var context = canvas.getContext('2d');
-  validateTest(1, numeroImagens, 'f2', canvas, context);
+  validateTest(1, numeroImagens, testSet, canvas, context);
+  if(testSet === 'f1'){
+    testSet = 'f2';
+  }else{
+    testSet = 'f1';
+  }
 });
 
-$('#train').click(function( event  ) {
+$('#train').click(function(event) {
   event.preventDefault();
   j++;
-  network = new Architect.Perceptron(1024, 512, 1);
-
+  network = new Architect.Perceptron(256, 257, 1);
   var trainingSet = [];
   var canvas = document.createElement('canvas');
   var context = canvas.getContext('2d');
   prepareImageArray(1, numeroImagens, 'f1', trainingSet, canvas, context, function(trainSet){
     console.log("Iniciou");
     network.train(trainSet, {
-      log: 1000,
-      iterations: 10000,
+      log: 5000,
+      iterations: 50000,
       error: 0.0000001,
       rate: 0.001
     });
